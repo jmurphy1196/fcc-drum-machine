1b17eb7259524a1419a1ad149dc514f7d8384b27
jmurphy1196 <jmurphy.1196@gmail.com>
2020-10-30 14:47:36 -0700

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { bankOne, bankTwo } from "./util/banks";
+import "./App.css";
 
 function App() {
+  const [currentBank, setCurrentBank] = useState(1);
+  //todo add bank toggle
+  useEffect(() => {
+    let btns = document.querySelectorAll(".drum-pad");
+    btns = Array.from(btns);
+    btns.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        btn.lastChild.play();
+        btn.style.filter = "brightness(70%)";
+        setTimeout(() => {
+          btn.style.filter = "brightness(100%)";
+        }, 100);
+      });
+    });
+    addKeyListeners();
+  }, []);
+  const soundEvent = (e) => {
+    let btns = document.querySelectorAll(".drum-pad");
+    btns = Array.from(btns);
+    let activeBank = currentBank === 0 ? bankOne : bankTwo;
+    activeBank.forEach((bank) => {
+      if (e.key === bank.keyTrigger.toLowerCase()) {
+        let sound = new Audio(bank.url);
+        sound.play();
+        btns.forEach((btn) => {
+          if (btn.textContent.toLowerCase() === e.key) {
+            btn.style.filter = "brightness(70%)";
+            setTimeout(() => {
+              btn.style.filter = "brightness(100%)";
+            }, 100);
+          }
+        });
+      }
+    });
+  };
+  const addKeyListeners = () => {
+    document.removeEventListener("keypress", soundEvent);
+    document.addEventListener("keypress", soundEvent);
+  };
+  console.log(currentBank);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="drum-machine">
+        <div id="display" className="drum-pad">
+          <button className="btn">Q</button>
+          <audio
+            id="Q"
+            className="clip"
+            src={currentBank === 0 ? bankOne[0].url : bankTwo[0].url}
+          />
+        </div>
+        <div className="drum-pad">
+          <button className="btn">W</button>
+          <audio
+            id="W"
+            className="clip"
+            src={currentBank === 0 ? bankOne[1].url : bankTwo[1].url}
+          />
+        </div>
+        <div className="drum-pad">
+          <button className="btn">E</button>
+          <audio
+            id="E"
+            className="clip"
+            src={currentBank === 0 ? bankOne[2].url : bankTwo[2].url}
+          />
+        </div>
+        <div className="drum-pad">
+          <button className="btn">A</button>
+          <audio
+            id="A"
+            className="clip"
+            src={currentBank === 0 ? bankOne[3].url : bankTwo[3].url}
+          />
+        </div>
+        <div className="drum-pad">
+          <button className="btn">S</button>
+          <audio
+            id="S"
+            className="clip"
+            src={currentBank === 0 ? bankOne[4].url : bankTwo[4].url}
+          />
+        </div>
+        <div className="drum-pad">
+          <button className="btn">D</button>
+          <audio
+            id="D"
+            className="clip"
+            src={currentBank === 0 ? bankOne[5].url : bankTwo[5].url}
+          />
+        </div>
+        <div className="drum-pad">
+          <button className="btn">Z</button>
+          <audio
+            id="Z"
+            className="clip"
+            src={currentBank === 0 ? bankOne[6].url : bankTwo[6].url}
+          />
+        </div>
+        <div className="drum-pad">
+          <button className="btn">X</button>
+          <audio
+            id="X"
+            className="clip"
+            src={currentBank === 0 ? bankOne[7].url : bankTwo[7].url}
+          />
+        </div>
+        <div className="drum-pad">
+          <button className="btn">C</button>
+          <audio
+            id="C"
+            className="clip"
+            src={currentBank === 0 ? bankOne[8].url : bankTwo[8].url}
+          />
+        </div>
+      </div>
     </div>
   );
 }
